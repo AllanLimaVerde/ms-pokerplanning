5520abfa36f55f26fd8f3123e9b8f81cd5aad86a
Allan LimaVerde <allan@provi.com.br>
2021-08-02 19:47:16 -0300

    Removing unnecessary code

--- controllers/updateRoom/userActions.js
+++ controllers/updateRoom/userActions.js
@@ -11,39 +11,6 @@ const { errors, roomStatuses } = require("../../constants")
  * @returns {Promise<Object>} Result
  */
 const userActions = {
-  selectCard: {
-    execute: 
-      async ({ userName, roomName, payload }) => {
-        // const room = roomService.hall()[roomName]
-
-        // if (!room) {
-        //   throw new Error(errors.roomNotFound)
-        // }
-
-        // if (!userName) {
-        //   throw new Error(errors.userRequired)
-        // }
-
-        // logger.info([`[Room ${roomName}]`, `User '${userName}' has selected card ${payload}`])
-
-        // const player = room.players.find(p => p.userName === userName)
-
-        // if (!player) {
-        //   throw new Error(errors.playerNotInRoom)
-        // }
-
-        // player.currentVote = payload
-        // console.log('uaegaeg', payload)
-
-        // roomService.checkIfRoomStatusShouldChange(roomName)
-      }
-  },
-  checkRoomForChange: {
-    execute:
-      async ({ userName, roomName }) => {
-        logger.info(`Player '${userName}' has pinged server for room '${roomName}' update`)
-      }
-  },
   resetRoomForNewVote: {
     execute:
       async ({ userName, roomName }) => {
